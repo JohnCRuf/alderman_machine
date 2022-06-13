4e492a3b5cd5584db46da140503fa9aed836022c
John <jcruf@uchicago.edo>
2022-06-13 11:17:32 -0500

    Added new data processing taks

--- tasks/elections_data_webscraping/code/chicago_elections_webscraping_fn.R
+++ tasks/elections_data_webscraping/code/chicago_elections_webscraping_fn.R
@@ -1,6 +1,4 @@
-link=links[1]
-
-table_scraper<-function(){
+table_scraper<-function(remDr,election,ward){
   doc <- htmlParse(remDr$getPageSource()[[1]])
   table<-readHTMLTable(doc)[[2]]
   col_odd<-seq_len(ncol(table))%%2
@@ -8,33 +6,32 @@ table_scraper<-function(){
   table2<-table2 %>%
     pivot_longer(!Precinct, names_to="Candidate", values_to="Votecount")
   table2<-table2 %>%
-    mutate(candidate=str_trim(str_to_title(candidate),side="right"))
+    mutate(Candidate=str_trim(str_to_title(Candidate),side="right"),
+           Election=election,
+           Ward=ward) %>%
+    filter(Precinct!="Total")
   return(table2)
 }
 
 chicago_elections_webscraper<-function(link) {
-  remDr <- remoteDriver(port = 4445L)
-  remDr$open()
-  remDr$navigate(link)
+  election_el<-remDr$findElement(using='xpath','//*[@id="page-wrap"]/div/div[4]/div[2]/form/table/tbody/tr[1]/td/b')
+  election<-election_el$getElementText()[[1]]
   dropdown <- remDr$findElement(using = 'xpath','//*[@id="race"]') #identify dropdown menu
   dropdown_text<-dropdown$getElementText()[[1]] #get dropdown options
   dropdown_text_vector<-str_split(dropdown_text,'\n')[[1]]
   alderman_options=which(str_detect(dropdown_text_vector,"Alderman")==T)
   ward_list<-parse_number(dropdown_text_vector[alderman_options])
-  data=data.frame(Precinct=integer(),Candidate=character(),Votecount=integer(),Ward=integer())
-  for(i in length(ward_list)){
-    xpath=paste0('//*[@id="race"]/option[',option[i],']')
+  data=data.frame(Precinct=integer(),Candidate=character(),Votecount=integer(),Election=character(),Ward=integer())
+  for(i in 1:length(ward_list)){
+    xpath=paste0('//*[@id="race"]/option[',alderman_options[i],']')
     option<-remDr$findElement(using = 'xpath',xpath)
     option$clickElement()
     submit <-remDr$findElement(using= 'xpath', '//*[@id="page-wrap"]/div/div[4]/div[2]/form/table/tbody/tr[4]/td/input')
     submit$clickElement()
-    data_table=table_scraper()
-    data_table=data_table %>%
-      mutate(Ward=ward_list[i])
-    data<-rbind(data,data_table)
+    data_table=table_scraper(remDr,election,ward_list[i])
+    data<-rbind(data_table,data)
     remDr$navigate(link)
   }
-remDr$close()
 return(data)
 }
 
