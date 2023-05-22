84711143580844e3ede1a34f04dd23d402d520bc
John Ruf <jcruf@uchicago.edu>
2023-05-21 21:46:24 -0500

    added new work to clean and process school/park data, double-dash-to data, and df_with_3_ands data

--- tasks/data_geolocate_menu/code/clean_menu_location.R
+++ tasks/data_geolocate_menu/code/clean_menu_location.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(stringr)
 library(readr)
 library(assertr)
-source("location_cleaning_fns.R")
 menu_df<-read_csv("../input/menu_df.csv")
 
 #Step 1: Apply manually edit locations with weird acronyms and typos
@@ -69,19 +68,51 @@ menu_df <- menu_df %>%
   filter(est_cost != 0) %>%
   filter(!str_detect(location, regex('not available', ignore_case = T)))
 
-#Step 2: Split location data into different standard formats
+#Step 2: Split location data into different standard formats and save to temp folder
 
 # --------------------
 # Location Data of Parks or Schools
 # --------------------
 school_park_df <- menu_df %>% 
     filter(str_detect(location, regex('school|park', ignore_case = T))) %>% #filter out any "st" or "av
-    filter(!str_detect(location, regex('st|av|rd|blvd|Lake Park|Central Park|lincoln park w', ignore_case = T))) %>% #fuilter out ON FROM TO
+    filter(!str_detect(location, regex('st|av|rd|blvd|Lake Park|Central Park|lincoln park w', ignore_case = T))) %>% #filter out ON FROM TO
     filter(!str_detect(location, regex('on|from|to', ignore_case = T))) %>%
     filter(!str_detect(location, regex('parkway', ignore_case = T)))
+
 leftover_df <- menu_df %>%
     anti_join(school_park_df)
 
+last_keyword_position <- function(x) {
+  keyword_positions <- unlist(str_locate_all(tolower(x), "\\bschool\\b|\\bpark\\b|\\bcenter\\b|\\field \\b||\\bcenter\\b"))
+  if (length(keyword_positions) == 0) return(as.integer(NA))
+  max(keyword_positions)
+}
+#eliminate implicit lists in location:
+school_park_df <- school_park_df %>%
+  rowwise() %>%
+  mutate(location_temp = strsplit(location, ",\\s*|\\s*&\\s*")) %>%
+  mutate(num_elements = length(location_temp)) %>%
+  unnest(location_temp) %>%
+  group_by(location_temp) %>%
+  mutate(est_cost =  est_cost / num_elements) %>%
+  ungroup() %>%
+  select(-num_elements)
+
+#remove irrelevant information
+school_park_df <- school_park_df %>%
+        mutate(location_2 = str_replace_all(location_temp, "\\(.*?\\)", ""), #remove text in parentheses
+               location_2 = str_replace(location_2, "(\\(TPC).*", ""), #remove TPC
+               location_2 = str_replace(location_2, "(\\)TPC).*", ""), #remove typo TPC
+               location_2 = str_replace(location_2, "Hih", "High"), #remove typo TPC
+               location_2 = ifelse(str_detect(location_2, " Campus Park"),
+                                   location_2, 
+                                   str_replace(location_2, " -.*", "")),
+               last_keyword_pos = map_int(location_2, last_keyword_position),
+               school_park_name = map2_chr(location_2, last_keyword_pos, ~ifelse(is.na(.y), NA_character_, str_sub(.x, end = .y)))) %>% #extract text before "school" or "park"
+#now we split the rows that contain multiple schools
+        select(-location_2, -last_keyword_pos, -location_temp)
+
+write.csv(school_park_df, "../temp/school_park_df.csv")
 # --------------------
 # Location Data of format "_ ST -- _ AV -to- _ ST"
 # --------------------
@@ -91,7 +122,18 @@ double_dash_to_df<-leftover_df %>%
 
 leftover_df <- leftover_df %>% 
   anti_join(double_dash_to_df)
-#double_dash_to_df <- double_dash_to_clean(double_dash_to_df)
+
+double_dash_to_df <- double_dash_to_df %>% 
+        mutate(#removing chicago street coordinates that confuse geolocator API
+                location_2 = str_replace_all(location, "\\(.*?\\)", ""),
+                main_street = str_extract(location_2, ".*(?=--)"), 
+                from_street = str_extract(location_2, "(?<=--).*(?=-to-)"), #extract text between "--" and "-to-", which is from street
+                to_street = str_extract(location_2, "(?<=-to-).*$"), #extract text after "-to-", which is to street
+                from_intersection = paste0(main_street, " and ", from_street), #paste to create intersections
+                to_intersection = paste0(main_street, " and ", to_street)) %>%
+        select(-location_2,-main_street, -from_street, -to_street)
+
+write.csv(double_dash_to_df, "../temp/double_dash_to_df.csv")
 # --------------------
 # Location Data of format "# N/S/E/W road_1 & N/S/E/W road_2 & N/S/E/W road_3 & N/S/E/W road_4"
 # --------------------
@@ -110,7 +152,29 @@ df_with_3_ands <- addition_modified_df %>%
 leftover_addition_df <- addition_modified_df %>%
   anti_join(df_with_3_ands)
 
-#df_with_3_ands <- triple_and_clean(df_with_3_ands)
+generate_intersections <- function(streets){
+  ns <- streets[substr(streets, 1, 1) %in% c("N", "S")]
+  ew <- streets[substr(streets, 1, 1) %in% c("E", "W")]
+  intersect_pairs <- expand.grid(ns, ew)
+  intersect_pairs <- paste(intersect_pairs$Var1, intersect_pairs$Var2, sep=" & ")
+  intersect_pairs
+}
+
+df_results<- df_with_3_ands %>%
+  mutate(id = row_number(),
+         location = str_split(location, " & ")) %>%
+  rowwise() %>%
+  mutate(location = list(generate_intersections(unlist(location)))) %>%
+  unnest(location) %>%
+  mutate(intersection_number = paste0("intersection_", ((row_number() - 1) %% 4) + 1)) %>%
+  pivot_wider(names_from = intersection_number, values_from = location)
+#add "id" column to df_with_3_ands
+df_with_3_ands <- df_with_3_ands %>%
+  mutate(id = row_number())
+df_with_3_ands <- left_join(df_with_3_ands, df_results) %>%
+  select(-id)
+  
+
 # --------------------
 # Location Data of format "# N/S/E/W road_1 & N/S/E/W road_2 & N/S/E/W road_3"
 # --------------------
@@ -119,7 +183,6 @@ df_with_2_ands <- leftover_addition_df %>%
 
 leftover_addition_df <- leftover_addition_df %>%
   anti_join(df_with_2_ands)
-
 
 # --------------------
 # Location Data of format "# N/S/E/W road_1 (& or ; or :) N/S/E/W road_2
@@ -142,16 +205,20 @@ leftover_addition_df <- leftover_addition_df %>%
 leftover_df <- leftover_df %>%
   anti_join(addition_df)
 rm(leftover_addition_df, addition_df, addition_modified_df)
+
 # --------------------
 # Location Data of format "# N/S/E/W _ ST"
 # --------------------
 normal_address_df <- leftover_df %>% 
   filter(!str_detect(location, "&|;|:|--|-and-|/")) %>%
   filter(str_detect(location,"^[0-9]{2,5} [N|S|E|W]"))
-normal_address_df <- normal_address_clean(normal_address_df)
 
 leftover_df <- leftover_df %>% 
   anti_join(normal_address_df)
+
+normal_address_df <- normal_address_df  %>%
+        mutate(address = str_replace_all(location, "\(.*?\)", ""))
+write.csv(normal_address_df, "../temp/normal_address_df.csv")
 
 #--------------------
 # Location Data of format "ON _ AV from _ ST to _ ST"
@@ -163,7 +230,14 @@ from_to_df <- leftover_df %>%
 leftover_df <- leftover_df %>%
     anti_join(from_to_df)
 
-ℓₖₙ_realized = ℓₖₙ ./ δ̄
+from_to_df <- from_to_df %>%
+    mutate(location_2 = str_replace_all(location, "\\(.*\\)", ""), #remove text in parentheses
+           from_street = str_extract(location_2, "(?<=from ).*(?= to)"), #extract text between "from" and "to", which is from street
+           to_street = str_extract(location_2, "(?<=to ).*$"),  #extract text after "to", which is to street
+           ) %>%
+    select(-location_2)
+
+write.csv(from_to_df, "../temp/from_to_df.csv")
 
 #--------------------
 # Location Data of format "##-### street
