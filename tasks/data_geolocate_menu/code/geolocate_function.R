9bcc176fe705dd900e1bccbac80f506794c1beb7
John Ruf <jcruf@uchicago.edu>
2023-06-10 12:39:20 -0500

    added code to locate all "normal address" formatted allocations

--- tasks/data_geolocate_menu/code/geolocate_function.R
+++ tasks/data_geolocate_menu/code/geolocate_function.R
@@ -1,15 +1,22 @@
-menu_geolocate <- function(df, var_name) {
+menu_geolocate <- function(df, var_name, batch_size) {
     df <- df %>%
         mutate(singlelineaddress = paste0(str_replace_all(!!sym(var_name), ",", " "), ", Chicago, IL"))
-    #use geocode_combine to geolocate df using the census api and save as "census_df"
-    results <- geocode_combine(df,
-    queries = list(
-        list(method = 'census'),
-        list(method = 'google')
-    ),
-    global_params = list(address = 'singlelineaddress'),
-    cascade = TRUE
-    ) %>%
+
+        # Split the data frame into chunks
+    chunks <- split(df, ceiling(seq_along(df[[var_name]])/batch_size))
+
+    # Apply geocode_combine to each chunk and combine results
+    results <- map_dfr(chunks, function(chunk) {
+        geocode_combine(chunk,
+        queries = list(
+            list(method = 'census'),
+            list(method = 'google')
+        ),
+        global_params = list(address = 'singlelineaddress'),
+        cascade = TRUE,
+        return_list = FALSE
+        )
+    }) %>%
         select(-singlelineaddress)
 
     return (results)
@@ -19,5 +26,5 @@ menu_geolocate <- function(df, var_name) {
 # library(tidyverse)
 # library(tidygeocoder)
 # main_df <- read_csv("../temp/normal_address_df.csv")
-# df <- main_df[1:10,]
-# geo_results <- menu_geolocate(df, "address")
+# test_df <- main_df[1:1000,]
+# geo_results <- menu_geolocate(test_df, "address", 500)
