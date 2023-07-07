339360226744f51562dc488933d141ef2b4e0d3f
John Ruf <jcruf@uchicago.edu>
2023-07-06 22:23:26 -0500

    addressed last of possible leftover formats

--- tasks/data_geolocate_menu/code/clean_menu_location.R
+++ tasks/data_geolocate_menu/code/clean_menu_location.R
@@ -35,11 +35,11 @@ location_replacements <- c(
 "Cicero/Lamon/George/Oakdale" = "N CICERO AVE & W OAKDALE AVE & N LAMON AVE & W GEORGE ST",
 "Altgeld/Deming/Lavergne/Leclaire" = "W ALTGELD ST & N LAVERGNE AVE & W DEMING PL & N LECLAIRE AVE",
 "Milwaukee/Barry/Springfield/Davlin" =  "N MILWAUKEE AVE & N DAVLIN CT & N SPRINGFIELD AVE & W BARRY AVE",
-"S WELLS ST & W 29 TH ST & S WENTWORTH AVE & S DAN RYAN WENTWORTH AV XR" = "S WELLS ST & W 29TH ST & S WENTWORTH AVE & 28TH ST", #nearest intersection
+"S WELLS ST & W 29TH ST & S WENTWORTH AVE & S DAN RYAN WENTWORTH AV XR" = "S WELLS ST & W 29TH ST & S WENTWORTH AVE & 28TH ST", #nearest intersection
 "ON FROM N BROADWAY (800 W) TO W DEVON AV (1216 W)" = "ON W SHERIDAN RD FROM N BROADWAY (800 W) TO W DEVON AV (1216 W)", #obvious typo based on coordinates
 "1501 W. Adams & about 1541-1600" = "1501-1600 W Adams ST", # appropriate format
 "AVE:6527 S MARSHFIELD AVE:6531 S MARSHFIELD AVE:6542 S MARSHFIELD AVE" = "6510-6542 S MARSHFIELD AVE", # appropriate format
-"Rhodes & 67 th & 67 th & S. Chicago" = "S RHODES AVE & E 67TH ST & E 67TH ST & S SOUTH CHICAGO AVE", 
+"Rhodes & 67th & 67th & S. Chicago" = "S RHODES AVE & E 67TH ST & E 67TH ST & S SOUTH CHICAGO AVE", 
 "19th & Allport; 17th & Loomis" = "S Loomis St & W 17th St, S Allport St & W 19th St", 
 "18th St / Blue Island & Loomis" = "18th ST & S LOOMIS ST", #triple intersection
 "18th ST/ 18th PL/Hoyne/Leavitt" = "W 18th ST & S HOYNE AVE & S LEAVITT ST & W 18th PL", # box of 4 intersections
@@ -54,7 +54,7 @@ location_replacements <- c(
 "Grand & Illinois between Orleans & LSD" = "ON W GRAND AVE FROM N ORLEANS ST TO N LAKE SHORE DR", # appropriate format
 "N Greenview/N Ashland/N Byron St" = "ON W BYRON ST FROM N GREENVIEW AVE TO N ASHLAND AVE", # appropriate format
 "IBeach access ramp & boardwalk-Jarvis & Lake &" = "MARION MAHONY GRIFFIN BEACH PARK", # nearest park, beach access map visible on google maps
-"78 th/79 th/Ridgeland/Creiger" = "E 78th ST & S RIDGELAND AVE & E 79th ST & S CREIGER AVE", # box of 4 intersections
+"78th/79th/Ridgeland/Creiger" = "E 78th ST & S RIDGELAND AVE & E 79th ST & S CREIGER AVE", # box of 4 intersections
 "Rogers/Kercheval to Caldwell/Kercheval/Kerbs to Rogers" = "ON N ROGERS AVE FROM N KERSCHEVAL AVE TO N CALDWELL AV ; ON N KERCHEVAL FROM N KERBS TO N ROGERS", # appropriate format
 "Wabash-Ohio & Onatrio;Dearborn Randolph & Lake" = "ON N WABASH AVE FROM E OHIO ST TO E ONTARIO ST; ON N DEARBORN ST FROM W RANDOLPH ST TO W LAKE ST", # 2 stretches of road
 "8001 Francisco" = "8001 S Francisco Ave",
@@ -62,19 +62,46 @@ location_replacements <- c(
 "Armitage,Clark,Cortland.Fullerton,Halsted" = "W ARMITAGE AVE & N CLARK ST & W CORTLAND ST & N HALSTED ST & W FULLERTON AVE",
 "Mural - Cicero Avenue viaduct adjacent to the North Branch Trail" = "N Forest Glen Ave & Cicero Ave", # closest intersection to the mural\
 "2728", "2728-2890 S State", # from bug in data processing
-"S SHORE DR 79 TH TO 80 TH" = "ON S SHORE DR FROM E 79TH ST TO E 80TH ST",
-"Luella E. 82 nd to E. 83 rd" = "ON S LUELLA AVE FROM E 82ND ST TO E 83RD ST",
+"S SHORE DR 79TH TO 80TH" = "ON S SHORE DR FROM E 79TH ST TO E 80TH ST",
+"Luella E. 82nd to E. 83rd" = "ON S LUELLA AVE FROM E 82ND ST TO E 83RD ST",
 "E107 Ewing to 1 st alley east" = "ON E 107TH ST FROM S EWING AVE TO S AVE J",
-"100 th St / from Ewing / Indianapolis" = "ON E 100TH ST FROM S EWING AVE TO S INDIANAPOLIS AVE",
+"100th St / from Ewing / Indianapolis" = "ON E 100TH ST FROM S EWING AVE TO S INDIANAPOLIS AVE",
 "Baltimore / from Brainard (13460 Baltimore)" = "13460 S Baltimore Ave",
-"13245 S GREEN BAY AVE:ON & S GREEN BAY AVE FROM & E 132 ND ST" = "13245 S GREEN BAY AVE",
+"13245 S GREEN BAY AVE:ON & S GREEN BAY AVE FROM & E 132ND ST" = "13245 S GREEN BAY AVE",
 "Jackson Blvd to Kilpatrick Av" = "Jackson Blvd & Kilpatrick Ave",
 "Mural at Sauganash Tr on Peterson Av" = "Saughnash Park", # nearest park
-"13245 S GREEN BAY AVE:ON & S GREEN BAY AVE FROM & E 132 ND ST" = "13245 S GREEN BAY AVE",
 "47th Street Viaduct- mural" = "47th and S Lake Park Ave", # nearest intersection
 "Viaduct Art Panel (Hyde Park) at 51st St." = "E Hyde Park Blvd & S Lake Park Ave", # nearest intersection
 "Dog Park across from Clarendon Park" = "Clarendon Dog Friendly Area",
-"Division, 2400 - 3200 Festival Lights" = "ON W DIVISION ST FROM N WESTERN AVE TO N KEDZIE AVE" #appropriate intersections for 2400 and 3200 addresses
+"Division, 2400 - 3200 Festival Lights" = "ON W DIVISION ST FROM N WESTERN AVE TO N KEDZIE AVE", #appropriate intersections for 2400 and 3200 addresses
+"ST (8400 S)" = "ON S WOLCOTT AVE FROM W 83RD ST (8300 S) TO W 84TH ST (8400 S)",
+"ROSCOE ST (3400 N)" = "ON N OCONTO AVE FROM W SCHOOL ST (3300 N) TO W ROSCOE ST (3400 N)",
+"AUGUSTA BLVD (1000 N)" = "ON N ST LOUIS AVE FROM W IOWA ST (900 N) TO W AUGUSTA BLVD (1000 N)",
+"NEWCASTLE AVE (6800 W)" = "ON W RASCHER AVE FROM W TALCOTT AVE (6740 W) TO N NEWCASTLE AVE (6800 W)",
+"HAMILTON AVE (2130 W)" = "ON W CORNELIA AVE FROM N HOYNE AVE (2100 W) TO N HAMILTON AVE (2130 W)",
+"S MORGAN ST (1000 W)" = "ON W MAXWELL ST FROM S BLUE ISLAND AVE (1133 W) TO S MORGAN ST (1000 W)",
+"KILDARE AV (4300 W)" = "ON W TAYLOR ST FROM S KEELER AV (4200 W) TO S KILDARE AV (4300 W)",
+"W AGATITE AVE (4430 N)" = "ON N KENTON AVE FROM W SUNNYSIDE AVE (4500 N) TO W AGATITE AVE (4430 N)",
+"RIDGE AV (5970 N)" = "ON N PAULINA ST FROM W THORNDALE AV (5840 N) TO W RIDGE AV (5970 N)",  
+"PALATINE AV (6243 N)" = "ON N NEWCASTLE AVE FROM W RAVEN ST (6142 N) TO W PALATINE AV (6243 N)",
+"N BROADWAY\n W ARDMORE AV (5800 N)" = "N BROADWAY & W ARDMORE AVE",
+"Pratt from 1000 west to Ridge" = "ON W PRATT BLVD FROM 1000 W TO N RIDGE BLVD",
+"43rd to 47th along Drexel Blvd" = "ON S DREXEL BLVD FROM E 43RD ST TO E 47TH ST",
+"18th St at Wolcott" = "W 18th ST & S WOLCOTT AVE",
+"69TH ST 224 E 86TH ST" = "224 E 86TH ST", #borrowed from previous entry
+"PINE GROVE AVE" = "N BROADWAY & W LELAND AVE; W WILSON AVE & N CLARENDON AVE; W ADDISON ST & N PINE GROVE AVE",
+"2-100 E CHICAGO AVE" = "ON E CHICAGO AVE FROM N STATE ST TO N RUSH ST", #closest intersections
+"s 176 E CHESTNUT ST" = "176 E CHESTNUT ST",
+"r 2861 N CLARK ST" = "2861 N CLARK ST", 
+"2225 - 2225 - SWABASHAVE" = "ON S WABASH AVE FROM E 22ND ST TO E 23RD ST",
+"Street Resurfacing - N. Sheridan (Devon)" = "N SHERIDAN RD & W DEVON AVE",
+"2728" = "ON S STATE ST FROM 2728 S TO 2890 S",
+"Chicago Ave @ Hoyne Ave. (cost shared with 1st ward)" = "Chicago Ave & Hoyne Ave",
+"Chicago Ave @ Hoyne Ave. (cost shared with 32nd ward)" = "Chicago Ave & Hoyne Ave",
+"Chicago Ave. at Hoyne Ave. (Crosswalk)" = "Chicago Ave & Hoyne Ave",
+"Clark St at Germania Pl" = "N Clark St & W Germania Pl",
+"E107 Ewing to 1 st alley east" = "ON E 107TH ST FROM S EWING AVE TO S AVE J", #closest intersection
+"63rd St (Wentworth - State)" = "ON W 63RD ST FROM S WENTWORTH AVE TO S STATE ST"
 )
 
 type_replacements <- c(
@@ -96,7 +123,15 @@ type_replacements <- c(
 # https://www.artworkarchive.com/profile/andy-bellomo/artwork/tunnel-of-blessings-neftali-reyes-jr-memorial-mural
 # https://chicago.suntimes.com/news/2021/7/14/22577773/neftali-reyes-dead-humboldt-park-mural-clemente-high-baseball-gun-violence-606-trial-bloomingdale'
 
-menu_df$location <- str_replace_all(menu_df$location, "\n", "")
+#eliminate \n in location
+while (any(str_detect(menu_df$location, "\n"))) {
+  menu_df$location <- str_replace_all(menu_df$location, "\n", "")
+}
+# while a double space is in location, replace with single space
+while (any(str_detect(menu_df$location, regex("  ", ignore_case = T)))) {
+  menu_df$location <- str_replace_all(menu_df$location, "  ", " ")
+}
+#apply replacement lists
 menu_df <- menu_df %>%
   mutate(
     # Use the lookup table for replacements in 'location'
@@ -104,15 +139,16 @@ menu_df <- menu_df %>%
     # Use the lookup table for replacements based on 'type'
     location = ifelse(type %in% names(type_replacements), type_replacements[type], location),
     # remove "corner of" from location
-    location = str_replace(location, ".*corner of", "")
+    location = str_replace(location, ".*corner of", ""),
+    # reformat POD camera relocations
+    location = str_replace(location, "Relocate (.*) to (.*)", "\\2"),
+    #get rid of POD camera at . or POD Camera - ignore case
+    location = str_replace(location, regex("POD Camera.*", ignore_case = T), ""),
   )
 
 # remove any spacing issues
 menu_df$location <- str_replace_all(menu_df$location, "([0-9])([A-Za-z])", "\\1 \\2")
-# while a double space is in location, replace with single space
-while (any(str_detect(menu_df$location, regex("  ", ignore_case = T)))) {
-  menu_df$location <- str_replace_all(menu_df$location, "  ", " ")
-}
+
 # delete any set of characters in between "(TPC" and ")"
 menu_df$location <- str_replace_all(menu_df$location, "\\(TPC.*\\)", "")
 # filter out all data with est_cost of 0 and location that contains "not available"
@@ -225,21 +261,25 @@ normal_address_df <- normal_address_df %>%
   mutate(address = str_replace_all(location, "\\(.*?\\)", ""))
 write.csv(normal_address_df, "../temp/normal_address_df.csv", row.names = F)
 
+leftover_df_2 <- leftover_df %>%
+  filter(str_detect(location, regex(" FROM ", ignore_case = T))) %>%
+  filter(str_detect(location, regex(" TO ", ignore_case = T))) 
+
+#create df with only location == "N MCCLURG CT FROM E NORTH WATER ST (430 N) TO E RIVER DR (404 N)"
+leftover_df_3 <- leftover_df %>%
+  filter(str_detect(location, regex("MCCLURG", ignore_case = T)))
 #--------------------
 # Location Data of format "ON _ AV from _ ST to _ ST"
 #--------------------
-on_from_to_replacements <- c("24 TH PL FROM & S ROCKWELL ST (2600 W) TO & S WASHTENAW AVE (2700 W)
-" = "ON W 24th PL FROM S ROCKWELL ST (2600 W) TO S WASHTENAW AVE (2700 W)",
+on_from_to_replacements <- c("24 TH PL FROM S ROCKWELL ST (2600 W) TO S WASHTENAW AVE (2700 W)" = "ON W 24th PL FROM S ROCKWELL ST (2600 W) TO S WASHTENAW AVE (2700 W)",
 "GREENVIEW-JARVIS TO HOWARD" = "ON N GREENVIEW AVE FROM W JARVIS AVE TO W HOWARD ST",
-"
-N MCCLURG CT FROM & E NORTH WATER ST (430 N) TO & E RIVER DR (404 N)" = "ON N MCCLURG CT FROM E NORTH WATER ST (430 N) TO E RIVER DR (404 N)",
-"N & W PRATT BLVD FROM 1051 W TO & N SHERIDAN RD (1200 )
-" = "ON W PRATT BLVD FROM 1051 W TO N SHERIDAN RD (1200 W)",
-"N & W SHERWIN AVE FROM 1200 W TO & N SHERIDAN RD (1300 )" = "ON N SHERWIN AVE FROM 1200 W TO N SHERIDAN RD (1300 W)",
-"N & W JARVIS AVE FROM & N BELL AVE (2200 W) TO & N OAKLEY VE (2300 W)" = "ON N JARVIS AVE FROM N BELL AVE (2200 W) TO N OAKLEY AVE (2300 W)",
+"N MCCLURG CT FROM E NORTH WATER ST (430 N) TO E RIVER DR (404 N)" = "ON N MCCLURG CT FROM E NORTH WATER ST (430 N) TO E RIVER DR (404 N)",
+"N W PRATT BLVD FROM 1051 W TO N SHERIDAN RD (1200 )" = "ON W PRATT BLVD FROM 1051 W TO N SHERIDAN RD (1200 W)",
+"N W SHERWIN AVE FROM 1200 W TO N SHERIDAN RD (1300 )" = "ON N SHERWIN AVE FROM 1200 W TO N SHERIDAN RD (1300 W)",
+"N W JARVIS AVE FROM N BELL AVE (2200 W) TO N OAKLEY VE (2300 W)" = "ON N JARVIS AVE FROM N BELL AVE (2200 W) TO N OAKLEY AVE (2300 W)",
 "Sherdian-Belmont to Diversey" = "ON N SHERIDAN RD FROM W BELMONT AVE TO W DIVERSEY PKWY",
 "S SHORE DR 79 TH TO 80 TH" = "ON S SHORE DR FROM E 79TH ST TO E 80TH ST",
-"N & N WOLCOTT AVE FROM & W CHASE AVE (7300 N) TO & N OGERS AVE (7340 N)" = "ON N WOLCOTT AVE FROM W CHASE AVE (7300 N) TO N ROGERS AVE (7340 N)",
+"N N WOLCOTT AVE FROM W CHASE AVE (7300 N) TO N OGERS AVE (7340 N)" = "ON N WOLCOTT AVE FROM W CHASE AVE (7300 N) TO N ROGERS AVE (7340 N)",
 "9500 to 9800 S. Emerald" = "ON S EMERALD AVE FROM W 95TH ST TO W 98TH ST",
 "9300 to 9500 S. Loomis" = "ON S LOOMIS ST FROM W 93RD ST TO W 95TH ST",
 "47 th Place - Western to Oakley" = "ON W 47TH PL FROM S WESTERN AVE TO S OAKLEY AVE",
@@ -253,36 +293,38 @@ N MCCLURG CT FROM & E NORTH WATER ST (430 N) TO & E RIVER DR (404 N)" = "ON N MC
 "N. HARDING-BLOOMINGDALE TO ARMITAGE" = "ON N HARDING AVE FROM W BLOOMINGDALE AVE TO W ARMITAGE AVE",
 "Luella E. 82 nd to E. 83 rd" = "ON S LUELLA AVE FROM E 82ND ST TO E 83RD ST",
 "Paulina - Moorman to Division" = "ON N PAULINA ST FROM W MOORMAN ST TO W DIVISION ST",
-"N & W SHERWIN AVE FROM 1200 W TO & N SHERIDAN RD (1300 )" = "ON N SHERWIN AVE FROM 1200 W TO N SHERIDAN RD (1300 W)",
+"N W SHERWIN AVE FROM 1200 W TO N SHERIDAN RD (1300 )" = "ON N SHERWIN AVE FROM 1200 W TO N SHERIDAN RD (1300 W)",
 "Belden - Normandy to Private Drive" = "ON W BELDEN AVE FROM N NORMANDY AVE TO 6440 W Belden Ave", #closest to private drive
+"Belden - Normandy to Private Dr" = "ON W BELDEN AVE FROM N NORMANDY AVE TO 6440 W Belden Ave", #closest to private drive
+"Belden - Normandy to Private Dr." = "ON W BELDEN AVE FROM N NORMANDY AVE TO 6440 W Belden Ave", #closest to private drive
 "W PATTERSON AVEDead End (4652 W)Dead End (4521 W)" = "ON W PATTERSON AVE FROM DEAD END (4652 W) TO DEAD END (4521 W)",
-"137 - 141 E 114 th Place (parkway)" = "ON E 114TH PL FROM S MICHIGAN AVE TO S INDIANA AVE",
+"137 - 141 E 114th Place (parkway)" = "ON E 114TH PL FROM S MICHIGAN AVE TO S INDIANA AVE",
 "Kostner - 47 th to 51 st" = "ON S KOSTNER AVE FROM W 47TH ST TO W 51ST ST",
-"W CARMEN AVE FROM & N RAVENSWOOD AVE (1800 W) TO & N ASHLAND AVE (1600 W)" = "ON W CARMEN AVE FROM N RAVENSWOOD AVE (1800 W) TO N ASHLAND AVE (1600 W)",
+"W CARMEN AVE FROM N RAVENSWOOD AVE (1800 W) TO N ASHLAND AVE (1600 W)" = "ON W CARMEN AVE FROM N RAVENSWOOD AVE (1800 W) TO N ASHLAND AVE (1600 W)",
 "Taylor Street-Western to Ogden" = "ON W TAYLOR ST FROM S WESTERN AVE TO S OGDEN AVE",
 "Taylor-Western to Ogden" = "ON W TAYLOR ST FROM S WESTERN AVE TO S OGDEN AVE",
 "Cottage Grove, 39 th to 51 st" = "ON S COTTAGE GROVE AVE FROM E 39TH ST TO E 51ST ST",
 "GREENVIEW-HOWARD TO JONQUIL TERRACE" = "ON N GREENVIEW AVE FROM W HOWARD ST TO W JONQUIL TER",
 "100 th St / from Ewing / Indianapolis" = "ON E 100TH ST FROM S EWING AVE TO S INDIANAPOLIS AVE",
-"Baltimore / from Brainard (13460 Baltimore)
-" = "ON S BALTIMORE AVE FROM W BRAINARD AVE TO 13460 S BALTIMORE AVE")
+"Baltimore / from Brainard (13460 Baltimore)" = "ON S BALTIMORE AVE FROM W BRAINARD AVE TO 13460 S BALTIMORE AVE")
 
-#replace leftover_df with new df
+#repeatedly remove \n from location until none remain
+# while (any(str_detect(leftover_df$location, "\n"))) {
+#   leftover_df$location <- str_replace_all(leftover_df$location, "\n", "")
+# }
 leftover_df <- leftover_df %>%
-mutate(
-location = ifelse(location %in% names(on_from_to_replacements), on_from_to_replacements[location], location)
-)
+  mutate(location = ifelse(location %in% names(on_from_to_replacements), on_from_to_replacements[location], location))
 
 from_to_df <- leftover_df %>%
   filter(str_detect(location, regex(" FROM ", ignore_case = T))) %>%
   filter(str_detect(location, regex(" TO ", ignore_case = T))) %>%
   filter(str_detect(location, regex("^ON ", ignore_case = T))) %>%
-  filter(!str_detect(location, regex(" from .* from ", ignore_case = T))) %>%
-  filter(!str_detect(location, regex(" to .* to ", ignore_case = T))) %>%
+  filter(!str_detect(location, regex(";", ignore_case = T))) %>%
   filter(!str_detect(location, "Relocate"))
 
 leftover_df <- leftover_df %>%
   anti_join(from_to_df)
+
 
 from_to_df <- from_to_df %>%
   mutate(
