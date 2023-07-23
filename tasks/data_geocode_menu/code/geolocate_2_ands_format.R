3a40644691c2d3ad213935a66cc2b36e74c13121
John Ruf <jcruf@uchicago.edu>
2023-07-23 18:27:17 -0500

    fixed 2_ands bug

--- tasks/data_geocode_menu/code/geolocate_2_ands_format.R
+++ tasks/data_geocode_menu/code/geolocate_2_ands_format.R
@@ -7,8 +7,6 @@ df <- read_csv("../input/df_with_2_ands.csv")
 geolocated_df <- menu_geolocate(df, "intersection_1", 100) %>%  #100 b/c of tougher geocoding.
     rename(lat_1 = lat, lon_1 = long, query_1 = query) %>%
     menu_geolocate(., "intersection_2", 100) %>% 
-    rename(lat_2 = lat, lon_2 = long, query_2 = query) %>%
-    menu_geolocate(., "intersection_3", 100) %>% 
-    rename(lat_3 = lat, lon_3 = long, query_3 = query) 
+    rename(lat_2 = lat, lon_2 = long, query_2 = query)
 
 write_csv(geolocated_df, "../output/geolocated_2_ands_df.csv")
