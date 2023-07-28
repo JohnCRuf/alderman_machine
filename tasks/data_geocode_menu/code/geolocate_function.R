6fd51b921dae4d18f9ca500cefefb133bb380c27
John Ruf <jcruf@uchicago.edu>
2023-07-27 21:48:53 -0500

    used new google-only geolocator which is far superior to census to locate everything

--- tasks/data_geocode_menu/code/geolocate_function.R
+++ tasks/data_geocode_menu/code/geolocate_function.R
@@ -30,7 +30,37 @@ menu_geolocate <- function(df, var_name, batch_size) {
 
     return (results)
 }
+menu_geolocate_googleonly <- function(df, var_name, batch_size) {
+    df <- df %>%
+        mutate(singlelineaddress = paste0(str_replace_all(!!sym(var_name), ",", " "), ", Chicago, IL"),
+        singlelineaddress = ifelse(is.na(!!sym(var_name)), NA, singlelineaddress))
+    #if var_name is NA, then singlelineaddress will be NA
+    #sort by singlelineaddress to maximize number of identical addresses per batch
+    df <- df[order(df$singlelineaddress),]
+        # Split the data frame into chunks
+    chunks <- split(df, ceiling(seq_along(df[[var_name]])/batch_size))
+    # Apply geocode_combine to each chunk and combine results
+    results <- map_dfr(chunks, function(chunk) {
+        geocode_combine(chunk,
+        queries = list(
+            list(method = 'google')
+        ),
+        global_params = list(address = 'singlelineaddress'),
+        cascade = TRUE,
+        return_list = FALSE
+        )
+    }) %>%
+        select(-singlelineaddress)
+    # Geocode "Chicago, IL"
+    chicago_df <- data.frame(address = 'Chicago, IL')
+    chicago_coords <- geocode(chicago_df, method = 'google', address = 'address')
+    # Replace with NA when the coordinates match that of "Chicago, IL"
+    results <- results %>%
+      mutate(lat = ifelse(lat == chicago_coords$lat & long == chicago_coords$long, NA, lat),
+             long = ifelse(long == chicago_coords$long & lat == chicago_coords$lat, NA, long))
 
+    return (results)
+}
 #comments to test code
 # library(tidyverse)
 # library(tidygeocoder)
