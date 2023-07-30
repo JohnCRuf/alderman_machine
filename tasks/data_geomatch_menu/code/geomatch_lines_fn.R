89293cefcbd5f42f9bc15db3870ffc3e4bd1423f
John Ruf <jcruf@uchicago.edu>
2023-07-30 17:44:07 -0500

    updated geomatching function to match by chunks and delete memory-hogging geometries throughout the process.

--- tasks/data_geomatch_menu/code/geomatch_lines_fn.R
+++ tasks/data_geomatch_menu/code/geomatch_lines_fn.R
@@ -9,13 +9,27 @@ create_sf_lines <- function(df, lat1, lon1, lat2, lon2, crs) {
   return(sf_object)
 }
 
-geomatch_lines <- function(lines, map) {
-  # Intersect the lines with the map
-  intersections <- st_intersection(lines, map)
+geomatch_lines <- function(lines, map,rows_per_chunk) {
+  #split lines into chunks of size rows_per_chunk to avoid memory issues
+  lines_split <- split(lines, ceiling(seq_along(lines$id)/rows_per_chunk))
+  #create empty dataframe to store results
+  df_line_matched <- data.frame()
+  #loop through each chunk
+  for (i in 1:length(lines_split)) {
+    #calculate the total length of each line before merge
+    lines_split[[i]] <- lines_split[[i]] %>%
+      mutate(total_length = st_length(geometry))
+    #run st_intersection on each chunk
+    intersections <- st_intersection(lines_split[[i]], map)
+    #calculate the length of each intersection and of each original line
+    intersections <- intersections %>%
+      mutate(intersect_length = st_length(geometry))
+    intersections <- intersections %>% select(-geometry) %>% as.data.frame()
+    #bind the results to df_line_matched
+    df_line_matched <- rbind(df_line_matched, intersections)
+  }
+  #suppress warnings
+  
 
-  # Calculate the length of each intersection and of each original line
-  intersections <- intersections %>%
-    mutate(intersect_length = st_length(geometry)) 
-
-  return(intersections)
+  return(df_line_matched)
 }
