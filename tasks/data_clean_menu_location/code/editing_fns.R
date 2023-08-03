e105393f59b0e53718e3fa53e166d0de56da00d2
John Ruf <jcruf@uchicago.edu>
2023-08-02 22:45:41 -0500

    improved editing fns

--- tasks/data_clean_menu_location/code/editing_fns.R
+++ tasks/data_clean_menu_location/code/editing_fns.R
@@ -6,22 +6,32 @@ last_keyword_position <- function(x) {
   max(keyword_positions)
 }
 
-replace_strings_in_df <- function(df, var, replace_vector, detect_vector, exact_match_vector) {
+replace_strings_in_df <- function(df, var, replace_vector = NA, detect_vector = NA, exact_match_vector = NA) {
   require(dplyr)
   require(purrr)
   require(stringr)
-
-  for(i in seq_along(detect_vector)){
-    df <- df %>% mutate({{var}} := if_else(str_detect({{var}}, names(detect_vector[i])), detect_vector[i], {{var}}))
+  
+  # Check if replace_vector is not NA
+  if(!is.na(replace_vector)) {
+    # Apply the replacements using the replace_vector
+    df <- df %>% mutate({{var}} := str_replace_all({{var}}, replace_vector))
   }
   
-
-  for(i in seq_along(exact_match_vector)){
-    df <- df %>% mutate({{var}} := if_else({{var}} == names(exact_match_vector[i]), exact_match_vector[i], {{var}}))
+  # Check if detect_vector is not NA
+  if(!is.na(detect_vector)) {
+    # Check for the detect_vector and replace those strings if detected
+    for(i in seq_along(detect_vector)){
+      df <- df %>% mutate({{var}} := if_else(str_detect({{var}}, names(detect_vector[i])), detect_vector[i], {{var}}))
+    }
   }
   
-
-  df <- df %>% mutate({{var}} := str_replace_all({{var}}, replace_vector))
+  # Check if exact_match_vector is not NA
+  if(!is.na(exact_match_vector)) {
+    # Apply exact matches from the exact_match_vector
+    for(i in seq_along(exact_match_vector)){
+      df <- df %>% mutate({{var}} := if_else({{var}} == names(exact_match_vector[i]), exact_match_vector[i], {{var}}))
+    }
+  }
   
   return(df)
 }
