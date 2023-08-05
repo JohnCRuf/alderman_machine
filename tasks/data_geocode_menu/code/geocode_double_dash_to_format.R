eeaba1a15076af055d3c1bf082768843aa53b1f9
John Ruf <jcruf@uchicago.edu>
2023-08-04 23:36:05 -0500

    corrected geocoding data format to comply with standard

--- tasks/data_geocode_menu/code/geocode_double_dash_to_format.R
+++ tasks/data_geocode_menu/code/geocode_double_dash_to_format.R
@@ -4,8 +4,8 @@ source("geocode_function.R")
 df <- read_csv("../input/double_dash_to_df.csv")
 
 geocoded_df <- menu_geocode(df, "from_intersection", 500) %>% #rename lat lat_from_intersection
-    rename(lat_from_intersection = lat, lon_from_intersection = long, query_from_intersection = query) %>%
+    rename(lat_1 = lat, lon_1 = long, query_1 = query) %>%
     menu_geocode(., "to_intersection", 500) %>% #rename lat lat_to_intersection
-    rename(lat_to_intersection = lat, lon_to_intersection = long, query_to_intersection = query) 
+    rename(lat_2 = lat, lon_2 = long, query_2 = query) 
 
 write_csv(geocoded_df, "../output/geocoded_double_dash_to_df.csv")
