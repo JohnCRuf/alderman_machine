96903755612ef29d95eff947ee7671819839080b
John Ruf <jcruf@uchicago.edu>
2023-08-06 19:26:55 -0500

    fixed formatting issue

--- tasks/data_geocode_menu/code/geocode_2_ands_format.R
+++ tasks/data_geocode_menu/code/geocode_2_ands_format.R
@@ -8,8 +8,7 @@ geocoded_df <- menu_geocode_googleonly(df, "intersection_1", 100) %>%  #100 b/c 
     rename(lat_1 = lat, lon_1 = long, query_1 = query) %>%
     menu_geocode_googleonly(., "intersection_2", 100) %>% 
     rename(lat_2 = lat, lon_2 = long, query_2 = query)
-
 #replace all obviously non-Chicago coordinates with NA
-geocoded_df <- filter_chicago_coordinates(df)
+geocoded_df <- filter_chicago_coordinates(geocoded_df)
 
 write_csv(geocoded_df, "../output/geocoded_2_ands_df.csv")
