7551e3bcb59fb3475c06c30ae98206483e20150f
John Ruf <jcruf@uchicago.edu>
2023-11-05 21:31:15 -0600

    added new task taht produces campaign contribution maps

---
+++ tasks/produce_contribution_maps/ReadMe.md
@@ -0,0 +1,16 @@
+# produce_contribution_maps
+    This task creates a set of figures illustrating ward-level campaign contributions for a given alderman.
+
+    “You take care of the people who take care of you — you know, the people who voted for you. That’s not Chicago politics, that’s Politics 101.” - Bernie Stone
+
+## Output
+* `contribution_map_(alderman)_ward_(#)_(year1_year2).png`: A map of ward in ward # with menu money spending from year1 to year2.
+
+## Code
+* `menu_title_mapper.R`: A set of function that maps arguments to detailed legends and titles for the maps.
+* `menu_money_map_production.R`: Creates a map of menu money spending for a given ward from year1 to year2.
+
+## Inputs
+* `incumbent_challenger_voteshare_df_precinct_level.csv`: Precinct level vote counts, ward level vote counts and shares, and an incumbency dummy for each candidate in each election.
+* `ward_precinct_menu_panel_2003_2011.rds`: A panel dataset of menu expenditures by ward, 2003-2011 precinct, and year.
+* `ward_precinct_menu_panel_2012_2022.rds`: A panel dataset of menu expenditures by ward, 2003-2011 precinct, and year.
---
+++ tasks/produce_contribution_maps/code/contribution_map_production.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(sf)
+library(assertthat)
+library(ggmap)
+library(ggplot2)
+library(stringr)
+library(viridis)
+library(labeling)
+source("contribution_title_mapper.R")
+# Read and clean data
+ARGS<- commandArgs(trailingOnly = TRUE) # nolint
+contribution_data <- readRDS(ARGS[1])
+ward_select <- ARGS[2]
+year_1 <- ARGS[3]
+year_2 <- ARGS[4]
+legend_setting <- "total"
+color_setting <- "viridis"
+break_size <- 15000
+break_max <- 150000
+output_file <- ARGS[5]
+
+#drop observations of menu data where year is greater than 2011 and ward != 50
+contribution_data <- contribution_data %>%
+  filter(year < year_2, year > year_1, ward_locate == ward_select)
+
+#take total spending by precinct
+contribution_spending_df <- contribution_data %>%
+    group_by(ward_locate, precinct_locate, geometry) %>%
+    summarize(contribution_spending = sum(total_contribution)) %>%
+    ungroup() %>%
+    mutate(observed_spending_fraction = contribution_spending/sum(contribution_spending))
+
+contribution_spending_map <- st_as_sf(contribution_spending_df)
+
+arg_to_legend_list <- arg_to_legend(legend_setting, year_1, year_2, contribution_spending_map)
+#breaks go from 0 to seq_max in increments of break_size
+breaks <- seq(0, break_max, break_size)
+colors <- arg_to_color(color_setting, breaks)
+df <- arg_to_legend_list[[1]]
+scale_name <- arg_to_legend_list[[2]]
+label_name <- arg_to_legend_list[[3]]
+
+figure <- ggplot() +
+  geom_sf(data = df, aes(fill = contribution_spending)) +
+  scale_fill_gradientn(name = scale_name, 
+                       colours = colors, 
+                       breaks = breaks, 
+                       labels = label_name,
+                       guide = guide_colorbar(ticks.colour = "black", ticks.linewidth = 0.5)) +
+  theme_void() +
+  labs(caption = "Source: Illinois State Board of Elections Website") +
+  theme(
+    plot.caption = element_text(hjust = 0.5, size = 12, margin = margin(t = 0, b = 0)),
+    plot.margin = margin(0, 0, 0, 0),  # top, right, bottom, left margins set to zero
+    legend.title = element_text(hjust = 0.5)  # horizontally center the legend title
+  )
+
+#save the map to output_file
+png(filename = output_file, width = 8, height = 8, units = "in", res = 300)
+print(figure)
+dev.off()
