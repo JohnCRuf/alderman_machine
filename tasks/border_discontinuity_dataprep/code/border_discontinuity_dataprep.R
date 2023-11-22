358e4b2c81e86486029479533918f088c291a7f2
John Ruf <jcruf@uchicago.edu>
2023-11-21 19:12:06 -0600

    rejiggerred old code to be functional with precinct level discontinuity analysis

--- tasks/border_discontinuity_dataprep/code/border_discontinuity_dataprep.R
+++ tasks/border_discontinuity_dataprep/code/border_discontinuity_dataprep.R
@@ -1,69 +1,46 @@
 library(sf)
+sf::sf_use_s2(FALSE) #disable s2 because it's not compatible with crappy ward maps
 library(dplyr)
 library(sp)
 source("../input/map_data_prep_fn.R")
 source("compute_border_distances.R")
 
 # Load 2012-2022 map
-map_2012 <- map_load("../temp/ward_precincts_2012_2022/ward_precincts_2012_2022.shp")
-
-map_2012_distances <- compute_precinct_distances(map_2012)
-
-# Load 2003-2011 map
-map_2003 <- map_load("../temp/ward_precincts_2003_2011/ward_precincts_2003_2011.shp")
-
-map_2003_distances <- compute_precinct_distances(map_2003)
-
-# add indicator for which map the data is from
-map_2003_distances$map <- "2003-2011"
-map_2012_distances$map <- "2012-2022"
-
-# rbind the two maps
-# remove shape area and shape length columns from 2012
-map_2012_distances <- map_2012_distances %>% 
-  select(-shape_area, -shape_len)
-map_distances <- rbind(map_2003_distances, map_2012_distances)
+ward_map_2012 <- readRDS("../input/ward_map_2012_2022.rds")
+block_map_2012 <- readRDS("../input/block_map_2010.rds")
+#sf merge within block_map and ward map, tell me what ward each block is within
+block_map_2012 <- st_join(block_map_2012, ward_map_2012, join = st_within)
+#remove blocks that are not in a ward
+block_map_2012 <- block_map_2012 %>% 
+  filter(!is.na(ward_locate))
+#filter to first 100 rows
+block_map_2012 <- block_map_2012 
+map_2012_distances <- compute_area_to_ward_distances(block_map_2012, ward_map_2012)
+#immediately write to RDS
+writeRDS(map_2012_distances, "../output/map_2012_distances.rds")
 
 #load needs data
-df <- read.csv("../input/ward_needs_data.csv")
+needs_df <- read.csv("../input/ward_needs_data.csv")
 #rename year map
-df <- df %>% 
+needs_df <- needs_df %>% 
   rename(map = year_range)
+#filter to 2012 map
+needs_df <- needs_df %>% 
+  filter(map == "2012-2022")
 #join by map and ward_locate
-map_distances_needs <- map_distances %>% 
-  left_join(df, by = c("map", "ward_locate")) 
+map_distances_needs <- map_2012_distances %>% 
+  left_join(needs_df, by = c("ward_locate")) 
 
-#load spending data
-spending_2003_2011 <- read.csv("../input/ward_precinct_menu_panel_2003_2011.csv")
-#if year is between 2003 and 2007, set cycle to 2003-2007. If year is between 2008 and 2011, set cycle to 2008-2011
-spending_2003_2011$cycle <- ifelse(spending_2003_2011$year %in% 2003:2007, "2003-2007", "2008-2011")
-#drop if year > 2012
-spending_2003_2011 <- spending_2003_2011 %>% 
-  filter(year <= 2012)
-#summarize by cycle, ward locate, precinct locate, and weighted_cost
-spending_2003_2011 <- spending_2003_2011 %>% 
-  group_by(cycle, ward_locate, precinct_locate) %>% 
-  summarize(weighted_cost = sum(weighted_cost))
 
 #load 2012-2022 spending data
-spending_2012_2022 <- read.csv("../input/ward_precinct_menu_panel_2012_2022.csv")
-#if year is between 2012 and 2015, set cycle to 2012-2015. If year is between 2016 and 2022, set cycle to 2016-2019, if year>2019 set cycle to 2020-2023
-spending_2012_2022$cycle <- ifelse(spending_2012_2022$year %in% 2012:2015, "2012-2015", ifelse(spending_2012_2022$year %in% 2016:2019, "2016-2019", "2020-2023"))
-#drop if year<2012
-spending_2012_2022 <- spending_2012_2022 %>% 
-  filter(year >= 2012)
-#summarize by cycle, ward locate, precinct locate, and weighted_cost
-spending_2012_2022 <- spending_2012_2022 %>% 
-  group_by(cycle, ward_locate, precinct_locate) %>% 
-  summarize(weighted_cost = sum(weighted_cost))
-#rbind spending data
-spending <- rbind(spending_2003_2011, spending_2012_2022)
-#create a column for the map based on cycle
-spending$map <- ifelse(spending$cycle %in% c("2003-2007", "2008-2011"), "2003-2011", "2012-2022")
+spending_2012_2022 <- read.csv("../input/block_menu_panel_2012_2022.csv")
+spending <- spending_2012_2022 %>% 
+  filter(cycle != "2004-2007" & cycle != "2008-2011")
+spending$tract_bloc <- as.character(spending$tract_bloc)
 
 #join spending data to map distances and needs data
 map_distances_needs_spending <- map_distances_needs %>% 
-  left_join(spending, by = c("map", "ward_locate", "precinct_locate"))
+  left_join(spending, by = c( "tract_bloc"))
 #remove geometry using sf
 map_distances_needs_spending <- st_drop_geometry(map_distances_needs_spending)
 #rename pct_of_needs to pct_of_needs_home
@@ -79,6 +56,9 @@ map_distances_needs_spending <- map_distances_needs_spending %>%
   rename(pct_of_needs_nearest = pct_of_needs)
 
 #create a new dataframe called ward_cycle_totals which is the total weighted_cost by ward_locate and cycle
+#merge spending with block_map_2012 to get ward_locate
+spending <- spending %>% 
+  left_join(block_map_2012, by = c("tract_bloc"))
 ward_cycle_totals <- spending %>% 
   group_by(ward_locate, cycle) %>% 
   summarize(total_wardlocate_spending = sum(weighted_cost))
--- tasks/border_discontinuity_dataprep/code/compute_border_distances.R
+++ tasks/border_discontinuity_dataprep/code/compute_border_distances.R
@@ -1,52 +1,43 @@
-compute_precinct_distances <- function(precincts) {
+compute_area_to_ward_distances <- function(small_map, wards_map) {
 
-    # Aggregate precincts into wards
-    wards_map <- precincts %>%
-    group_by(ward_locate) %>%
-    summarise(geometry = st_union(geometry))
-    crs_precincts <- st_crs(precincts)
+    crs_small_map <- st_crs(small_map)
     crs_wards_map <- st_crs(wards_map)
 
-    if (crs_precincts != crs_wards_map) {
-    wards_map <- st_transform(wards_map, crs_precincts)
+    if (crs_small_map != crs_wards_map) {
+    wards_map <- st_transform(wards_map, crs_small_map)
     }
 
     # Find the nearest ward for each precinct and calculate distance
     # Initialize a dataframe to store the results
-    precinct_data <- data.frame(nearest_ward = character(nrow(precincts)), 
-                                distance_to_ward = numeric(nrow(precincts)), 
-                                border_precinct = numeric(nrow(precincts)),
+    small_unit_data <- data.frame(nearest_ward = character(nrow(small_map)), 
+                                distance_to_ward = numeric(nrow(small_map)), 
+                                border_precinct = numeric(nrow(small_map)),
                                 stringsAsFactors = FALSE)
 
     # Iterate over each precinct shape
-    for (i in seq_len(nrow(precincts))) {
+    for (i in seq_len(nrow(small_map))) {
     # save the geometry of the current precinct
-    geom <- precincts$geometry[i]
+    #print i
+    print(i)
+    geom <- small_map$geometry[i]
 
     # Exclude the current ward from the search
-    current_ward <- precincts$ward_locate[i]
+    current_ward <- small_map$ward[i]
     other_wards <- wards_map[wards_map$ward_locate != current_ward, ]
 
     # Calculate nearest ward and distance
     distances <- st_distance(other_wards, geom)
-    nearest_ward_idx <- which.min(distances)
+    nearest_ward_idx <- which.min(distances)[1]
     nearest_ward <- other_wards$ward_locate[nearest_ward_idx]
     distance_to_ward <- as.numeric(distances[nearest_ward_idx])
-    #create a dummy variable if the precinct is a border precinct
-    if (distance_to_ward == 0) {
-        border_precinct <- 1
-    } else {
-        border_precinct <- 0
-    }
 
     # Store results
-    precinct_data$nearest_ward[i] <- nearest_ward
-    precinct_data$distance_to_ward[i] <- distance_to_ward
-    precinct_data$border_precinct[i] <- border_precinct
+    small_unit_data$nearest_ward[i] <- nearest_ward
+    small_unit_data$distance_to_ward[i] <- distance_to_ward
     }
 
-    # Bind the precincts data
-    precinct_df <- cbind(precincts, precinct_data)
+    # Bind the small_map data
+    final_df <- cbind(small_map, small_unit_data)
 
-    return(dataframe = precinct_df)
+    return(dataframe = final_df)
 }
