726b503487e95f9cfb1c551d7b5cad731701d283
John Ruf <jcruf@uchicago.edu>
2023-11-30 00:52:14 -0600

    committing writing improvements

--- tasks/model_exhibit_did/code/exhibit_precinct_variation.R
+++ tasks/model_exhibit_did/code/exhibit_precinct_variation.R
@@ -8,16 +8,14 @@ agg_results_df <- agg_results_df %>%
     mutate(Group = ifelse(grepl("top", filename), "top", "bottom"),
     precinct = as.numeric(str_extract(filename, "(?<=_)[0-9]+(?=_)")))
 
-plot <- ggplot(agg_results_df, aes(x = precinct, y = att, ymin = lower_ci, ymax = upper_ci, color = Group, linetype = Group, shape = Group)) +
+plot <- ggplot(agg_results_df, aes(x = precinct, y = att, ymin = lower_ci, ymax = upper_ci, color = Group, shape = Group)) +
     geom_point(size = 3) +
-    geom_line() +  # Add line to connect points
     geom_errorbar() +
     geom_hline(yintercept = 0, linetype = "dashed") +
     labs(x = "Precinct Count", y = "Average Treatment Effect on Treated (pp)") +
     theme_bw() +
     theme(legend.position = "bottom") +
-    scale_color_manual(values = c("red","blue"), labels = c("Least supporting precincts", "Most supporting precincts")) +
-    scale_shape_manual(values = c(19, 17), labels = c("Least supporting precincts", "Most supporting precincts")) +
-    scale_linetype_manual(values = c("solid", "dashed"), labels = c("Least supporting precincts", "Most supporting precincts"))
+    scale_color_manual(values = c("blue","red"), labels = c("Least supporting precincts", "Most supporting precincts")) +
+    scale_shape_manual(values = c(19, 17), labels = c("Least supporting precincts", "Most supporting precincts"))
 
 ggsave(plot, filename = ARGS[2], width = 8, height = 6, units = "in", dpi = 300)
